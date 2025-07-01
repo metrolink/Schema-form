98ae7899d4311c3bddb5bc86d35dc02d3087600f
Tor Andreas Røsæg <to.an@hotmail.no>
2025-07-01 14:09:24 +0200

    added user input checks

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,69 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        settings: {
+            react: {
+                version: 'detect', // Automatically detect the React version
+            },
+        },
+        rules: {
+            semi: 'error',
+            quotes: [ 'error', 'single' ],
+            'space-before-function-paren': [
+                'error',
+                {
+                    anonymous: 'always',
+                    named: 'never',
+                    asyncArrow: 'always'
+                }
+            ],
+            'space-before-blocks': 'warn',
+            'no-trailing-spaces': 'warn',
+            'keyword-spacing': [
+                'error',
+                {
+                    before: true,
+                    after: true
+                }
+            ],
+            curly: 'error',
+            'arrow-parens': 'warn'
+        }
+    }, {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        languageOptions: { globals: globals.browser }
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    tseslint.configs.recommended,
+    {
+        ...pluginReact.configs.flat.recommended,
+        settings: {
+            react: {
+                pragma: 'React', // Pragma to use, default to 'React'
+                version: 'detect', // Automatically detect the React version
+            },
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off', // Disable the rule for React 17+
+        },
+    }, {
+        files: ['**/*.json'],
+        plugins: { json },
+        language: 'json/json',
+        extends: ['json/recommended']
+    }, {
+        files: ['**/*.jsonc'],
+        plugins: { json },
+        language: 'json/jsonc',
+        extends: ['json/recommended']
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+]);
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,21 +1,96 @@
+import { useState } from 'react';
 import electricity from '../components/electricity.json';
 
+type fieldVariables = {
+    label: string,
+    type: string,
+    name?: string,
+    fieldName?: string,
+    options?: optionVariables[],
+    required?:boolean,
+}
+
+type optionVariables = {
+    value: string
+}
+
 function MyForm() {
+    const [addressError, setAddressError] = useState<string>('')
+    const [nameError, setNameError] = useState<string>('')
+    const [emailError, setEmailError] = useState<string>('')
 
     const json = JSON.stringify(electricity);
     const fullJson = JSON.parse(json);
 
-    function logAnswer(submit){
-        const form = submit.get("property_type")
-        console.log(form)
+    function validateAnswer(submit:any){
+        submit.preventDefault();
+
+        const form = submit.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+
+        for (let index = 0; index < 3; index++) {
+            const formJsonName = fullJson.fields[index].name ?? fullJson.fields[index].fieldName
+            validateUserInput(formJsonName, formJson[formJsonName])
+        }
+
+
+        
+    }
+
+    function validateUserInput(field:string, userInput:any){
+        switch(field){
+            case "property_type":
+                break;
+            case "address_street_name":
+                const re = /[a-zA-ZæøåÆØÅ]+( )\d/
+                const check = re.test(userInput);
+                console.log(userInput);
+                if(!check){
+                    setAddressError('Does not contain an address with number or contain special characters')
+                }
+                else{setAddressError('')}
+                break;
+            case "name":
+                const reName = /^[a-zA-Z ]+$/
+                const checkName = reName.test(userInput)
+                console.log(userInput);
+                if(!checkName){
+                    setNameError('Name must only contain characters')
+                }
+                else{setNameError('')}
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    function generateFields(field:fieldVariables){
+        if(field.type == 'select'){
+            return <div>
+                <label key={field.label}>{field.label}: 
+                <select name={field.name || field.fieldName} required={field.required} key={field.name}>
+                    {field.options && field.options.map((option:optionVariables) => <option key={option.value} value={option.value}>{option.value}</option>)}
+                </select>
+                </label>
+            </div>
+        }
+        else{
+            return <div><label key={field.label}>{field.label}: <input type={field.type} name={field.name ?? field.fieldName} required={field.required} /></label></div>
+        }
     }
     return(
     <div>
-        <form action={logAnswer}>
-        <h1>{fullJson.title}</h1>
-        {fullJson.fields.map((field) => <div><label>{field.label}</label><input type={field.type} name={field.name} required={field.required}></input></div>)}
-        <button type='submit'> a</button>
+        <form onSubmit={validateAnswer}>
+        <div>{fullJson.title}</div>
+        {fullJson.fields.map((field:fieldVariables) => 
+        <div key={field.name || field.fieldName}>{generateFields(field)}</div>
+    )}
+        <button type='submit'> Send in</button>
         </form>
+        {addressError && <div>{addressError}<br /></div>}
+        {nameError && <div>{nameError}<br /></div>}
     </div>
 )
 }
